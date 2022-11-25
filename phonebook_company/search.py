02df6b82f8603aaa45aed75f20c0dd2c2590f33c
Igoryani4 <110982221+Igoryani4@users.noreply.github.com>
2022-11-25 13:39:38 +0300

    Update search.py

--- phonebook_company/search.py
+++ phonebook_company/search.py
@@ -25,10 +25,22 @@ def update_car_number():
     cr.retrive(car_number=search)
     user_id = ui.input_id()
     lg.logging.info('User entered: {user_id}')
+    new_number = ui.input_new_car_number()
+    lg.logging.info('User entered: {new_number}')
+    cr.update(id = user_id, new_car_number = new_car_number)
+
+    
+def update_phone_number():
+    lg.logging.info('The user has selected item number 6.3')
+    search = ui.input_phone_number()
+    lg.logging.info('User entered: {search}')
+    cr.retrive(number = search)
+    user_id = ui.input_id()
+    lg.logging.info('User entered: {user_id}')
     new_number = ui.input_new_number_phone()
     lg.logging.info('User entered: {new_number}')
-    cr.update(id=user_id, new_number = new_number)
-
+    cr.update(id=user_id, new_number=new_number)
+    
     
 
     
