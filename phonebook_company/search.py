437a9020d019bbba1fab3682bbeeed1214a5f0d1
Igoryani4 <110982221+Igoryani4@users.noreply.github.com>
2022-11-25 10:59:29 +0300

    Update search.py

--- phonebook_company/search.py
+++ phonebook_company/search.py
@@ -6,12 +6,12 @@ import user_interface as ui
 
 def search_surname():
     lg.logging.info('The user has selected item number 6.1')
-    search = input('Введите фамилию: ')
+    search = ui.input_surname()
     lg.logging.info('User entered: {search}')
     cr.retrive(surname=search)
-    user_id = input('Введите id записи: ')
+    user_id = ui.input_id()
     lg.logging.info('User entered: {user_id}')
-    new_number = input('Введите новый номер телефона: ')
+    new_number = ui.input_new_number_phone()
     lg.logging.info('User entered: {new_number}')
     cr.update(id=user_id, new_number=new_number)
 
@@ -23,8 +23,8 @@ def search_car_number():
     search = ui.input_car_number()
     lg.logging.info('User entered: {search}')
     cr.retrive(car_number=search)
-    user_id = input('Введите id записи: ')
+    user_id = ui.input_id()
     lg.logging.info('User entered: {user_id}')
-    new_number = input('Введите новый номер телефона: ')
+    new_number = ui.input_new_number_phone()
     lg.logging.info('User entered: {new_number}')
     cr.update(id=user_id, new_number=new_number)
